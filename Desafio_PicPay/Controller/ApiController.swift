d5967de75b459755372b317a6dfd60f82d969b2e
Denis Janoto <denis.janoto@icloud.com>
2020-05-24 20:26:47 -0300

    First Commit

---
+++ Desafio_PicPay/Controller/ApiController.swift
@@ -0,0 +1,9 @@
+//
+//  ApiController.swift
+//  Desafio_PicPay
+//
+//  Created by Denis Janoto on 24/05/20.
+//  Copyright © 2020 Denis Janoto. All rights reserved.
+//
+
+import Foundation
---
+++ Desafio_PicPay/View/CustomViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomViewCell.swift
+//  Desafio_PicPay
+//
+//  Created by Denis Janoto on 24/05/20.
+//  Copyright © 2020 Denis Janoto. All rights reserved.
+//
+
+import UIKit
+
+class CustomViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
